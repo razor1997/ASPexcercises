46d64a9b473e76d4a6a1e6f7e1eef64334497040
Konrad <webmastering1997@gmail.com>
2019-03-23 21:42:44 +0100

    ASP Excercises 2

---
+++ Projekt/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Projekt.Models;
+
+namespace Projekt.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greetings = hour > 17 ? "Dzień dobry" : "Dobry wieczór";
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GueastResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+                return View("Thanks", guestResponse);
+            else
+                return View();
+        }
+    }
+}
---
+++ Projekt/Models/GueastResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace Projekt.Models
+{
+    public class GueastResponse
+    {
+        [Required(ErrorMessage ="Proszę podać swoje imię i nazwisko.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Proszę podać swój email adres")]
+        [RegularExpression(".+\\@.+\\..+",
+            ErrorMessage ="Proszę podać prawidłowy adres e-mail")]
+        public string Email { get; set; }
+        [Required(ErrorMessage ="Podaj swój numer telefonu")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage ="Proszę się okreslić co Pana/Panią interesuje")]
+        public bool? WillAttend { get; set; }
+    }
+} 
